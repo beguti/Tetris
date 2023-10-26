125201eece42de00a7d302a6aa472d20ef421a9f
Begüm İ <begumibrisim@gmail.com>
2023-10-26 18:13:00 +0300

    Tetris oyunu güncellendi.

--- Tetris/Assets/Scripts/GameManager.cs
+++ Tetris/Assets/Scripts/GameManager.cs
@@ -4,15 +4,181 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager Instance { get; private set; }
+
+    public MyBlockController Current { get; set; }
+
+    private const int GridSizeX = 10;
+    private const int GridSizeY = 20;
+
+    public bool[,] Grid = new bool[GridSizeX, GridSizeY];
+
+    public float GameSpeed => gameSpeed;
+
+    [SerializeField, Range(.1f, 1f)] float gameSpeed = 1;
+
+    [SerializeField] List<MyBlockController> listPrefabs;
+
+    private List<MyBlockController> _listHistory = new List<MyBlockController>();
+
+    #region Test
+
+    public bool IsOpenTest;
+
+    [SerializeField] SpriteRenderer displayDataPrefabs;
+
+    private SpriteRenderer[,] previewDisplay = new SpriteRenderer[GridSizeX, GridSizeY];
+
+    private void UpdateDisplayPreview()
     {
-        
+        if (!IsOpenTest) return;
+        for (int i = 0; i < GridSizeX; i++)
+        {
+            for (int j = 0; j < GridSizeY; j++)
+            {
+                var active = Grid[i, j];
+                var sprite = previewDisplay[i, j];
+
+                sprite.color = active ? Color.green : Color.red;
+
+                var color = sprite.color;
+                color.a = .1f;
+                sprite.color = color;
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    #endregion
+
+    private void Awake()
     {
-        
+        Instance = this;
+
+        if (IsOpenTest)
+        {
+            for (int i = 0; i < GridSizeX; i++)
+            {
+                for (int j = 0; j < GridSizeY; j++)
+                {
+                    var sprite = Instantiate(displayDataPrefabs, transform);
+                    sprite.transform.position = new Vector3(x: 1, y: 1, z: 0);
+                    previewDisplay[i, j] = sprite;
+                }
+            }
+        }
     }
+
+    private void Start()
+    {
+        Spawn();
+    }
+
+    public bool IsInside(List<Vector2> listCoordinate)
+    {
+        foreach (var coordinate in listCoordinate)
+        {
+            int x = Mathf.RoundToInt(coordinate.x);
+            int y = Mathf.RoundToInt(coordinate.y);
+
+            if (x < 0 || x >= GridSizeX)
+            {
+                //Horizontal out
+                return false;
+            }
+
+            if (y < 0 || y >= GridSizeY)
+            {
+                //Vertical out
+                return false;
+            }
+
+
+            if (Grid[x, y])
+            {
+                //Hit something
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public void Spawn()
+    {
+        var index = Random.Range(0, listPrefabs.Count);
+        var blockController = listPrefabs[index];
+        var newBlock = Instantiate(blockController);
+        Current = newBlock;
+
+        _listHistory.Add(newBlock);
+
+        UpdateDisplayPreview();
+    }
+
+    private bool IsFullRow(int index)
+    {
+        for (int i = 0; i < GridSizeX; i++)
+        {
+            if (!Grid[i, index])
+                return false;
+        }
+
+        return true;
+    }
+
+    public void UpdateRemoveObjectController()
+    {
+        for (int i = 0; i < GridSizeY; i++)
+        {
+            var isFull = IsFullRow(i);
+            if (isFull)
+            {
+                //Remove
+                foreach (var myBlock in _listHistory)
+                {
+                    var willDestroy = new List<Transform>();
+                    foreach (var piece in myBlock.listPiece)
+                    {
+                        int y = Mathf.RoundToInt(piece.position.y);
+                        if(y==i)
+                        {
+                            //Add Remove
+                            willDestroy.Add(piece);
+                        }
+                        else if(y>i)
+                        {
+                            var position=piece.position;
+                            position.y--;
+                            piece.position = position;
+                        }
+                    }
+                    //Remove
+                    foreach (var item in willDestroy)
+                    {
+                        myBlock.listPiece.Remove(item);
+                        Destroy(item.gameObject);
+                    }
+                    
+                }
+
+
+                //ChangeData
+                for (int j = 0; j < GridSizeX; j++)
+                    Grid[j, i] = false;
+
+                for (int j = i + 1; j < GridSizeY; j++)
+                {
+                    for (int k = 0; k < GridSizeX; k++)
+                    {
+                        Grid[k, j - 1] = Grid[k, j];
+                    }
+                }
+
+                //Coll Again
+                UpdateRemoveObjectController();
+                return;
+            }
+        }
+    }
+
 }
---
+++ Tetris/Assets/Scripts/MyBlockController.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyBlockController : MonoBehaviour
+{
+    public List<Transform> listPiece => ListPiece;
+    [SerializeField] List<Transform> ListPiece = new List<Transform>();
+
+    private void Start()
+    {
+        GameManager.Instance.Current = this;
+        StartCoroutine(MoveDown());
+    }
+
+    IEnumerator MoveDown()
+    {
+        while (true)
+        {
+            var delay = GameManager.Instance.GameSpeed;
+            yield return new WaitForSeconds(delay);
+
+            var isMovable = GameManager.Instance.IsInside(GetPreviewPosition());
+            if (isMovable)
+                Move();
+            else
+            {
+                foreach (var piece in listPiece)
+                {
+                    int x = Mathf.RoundToInt(piece.position.x);
+                    int y = Mathf.RoundToInt(piece.position.y);
+
+                    //if (y >= GameManager.Instance.Grid.GetLength(1))
+                    //{
+                    //    //GameOver
+                    //    GameManager.Instance.OnGameOver();
+                    //    yield break;
+                    //}
+
+                    GameManager.Instance.Grid[x, y] = true;
+                }
+
+                GameManager.Instance.UpdateRemoveObjectController();
+
+                GameManager.Instance.Spawn();
+
+                break;
+            }
+        }
+    }
+
+    private List<Vector2> GetPreviewPosition()
+    {
+        var result = new List<Vector2>();
+        foreach (var piece in ListPiece)
+        {
+            var position = piece.position;
+            position.y--;
+            result.Add(position);
+        }
+        return result;
+    }
+
+    private void Move()
+    {
+        var position = transform.position;
+        position.y--;
+        transform.position = position;
+    }
+}
